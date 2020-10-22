eec74589c844b808bb98945b2e02c406bd7ddd02
mohitjoshi81 <59601077+mohitjoshi81@users.noreply.github.com>
2020-10-22 10:25:13 +0530

    Add files via upload

---
+++ Retailer_startup_screen.java
@@ -0,0 +1,18 @@
+package com.example.atmanirbhar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Retailer_startup_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retailer_startup_screen);
+
+
+    }
+}
